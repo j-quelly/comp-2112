9c04c91f23fdad1fb764e9b9abfc97342b11d48a
Jamie Kelly <jamie@studio174.ca>
2016-01-19 19:20:11 -0800

    updates client-side functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /**
- * unobtrusive client-side scripting for comp 2112
+ * Unobtrusive client-side scripting for comp 2112
  */
 
 // store form object in a var to reference later
@@ -14,10 +14,94 @@ searchForm.onsubmit = function(event) {
     // capture form data
     var data = document.getElementById('q').value;
 
+    // assemble the string for better search results
+    data = assembleQuery(data);
+
 
     /**
-     * prepare the string for better search results
+     * GET data
      */
+
+    // get current weather data
+    var current = getCurrent(data);
+    var forecast = getForecast(data);
+
+
+    /**
+     * Manipulate the DOM
+     */
+
+    // change the background colour
+    document.getElementById('search').id = 'results';
+
+    // hide the search field
+    document.getElementById('search-container').className = 'hidden';
+
+    // display a button to clear search results
+    var clearBtn = document.getElementById('clear-results');
+    clearBtn.className = '';
+    clearBtn.className = 'btn right';
+
+    // display the results
+
+
+}
+
+
+/**
+ * functions
+ */
+
+// a function to GET weather data
+function getCurrent(arg) {
+    // vars
+    var xhr,
+        appid = '5ec25e4aa9a5d4e41d545224fcf5d367';
+
+    // create a new ajax request (XML HTTP request)
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    // specify the type of request
+    xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + arg + '&APPID=' + appid, false);
+
+    // send the request to the server
+    xhr.send();
+
+    // return the data
+    return xhr.responseText;
+}
+
+// a function to GET the forecast
+function getForecast(arg, count) {
+    // vars
+    var xhr,
+        appid = '5ec25e4aa9a5d4e41d545224fcf5d367';
+
+    // create a new ajax request (XML HTTP request)
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    // specify the type of request
+    xhr.open('GET', 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + arg + '&cnt=' + count + '&APPID=' + appid, false);
+
+    // send the request to the server
+    xhr.send();
+
+    // return the data
+    return xhr.responseText;
+}
+
+// a function to prepare the string for better search results
+function assembleQuery(data) {
 
     // if the captured form data contains a comma
     if (data.indexOf(',') > -1) {
@@ -34,36 +118,5 @@ searchForm.onsubmit = function(event) {
         data = data.replace(' ', '');
     }
 
-
-    /**
-     * GET data
-     */
-
-    // vars
-    var xhr,
-        appid = '5ec25e4aa9a5d4e41d545224fcf5d367';
-
-    // create a new ajax request
-    if (window.XMLHttpRequest) {
-        xhr = new XMLHttpRequest();
-    } else {
-        // code for IE6, IE5
-        xhr = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-
-    // specify the type of request
-    xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + data + '&APPID=' + appid, false);
-
-    // send the request to the server
-    xhr.send();
-
-    console.log(xhr.status);
-    console.log(xhr.statusText);
-
-
-    /*    // GET data
-        
-        // api.openweathermap.org/data/2.5/weather?q='.$q.'&APPID='.$appid 
-        // api.openweathermap.org/data/2.5/forecast/daily?q='.$q.'&cnt=6&APPID='.$appid);
-    */
+    return data;
 }
